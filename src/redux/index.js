c31ebc537d321448df036e9495f43ed6ac306525
Sriramshankar123 <92223884+Sriramshankar123@users.noreply.github.com>
2023-09-08 05:01:10 +0530

    First commit

---
+++ src/redux/index.js
@@ -0,0 +1,6 @@
+import handleCart from "./action/reducers/handleCart";
+import { combineReducers} from "redux";
+const rootReducers= combineReducers({
+    handleCart,
+})
+export default rootReducers;
